f658bac21fb825f6192007caaaabd585ed9c746e
Alex Lu <95luyao@gmail.com>
2016-06-05 01:53:03 +0800

    accuracy to 98%

--- demo/src/classification_famous.py
+++ demo/src/classification_famous.py
@@ -22,10 +22,12 @@ for elem in a:
     if int(elem[2])<2001 and int(elem[2])>=1995:
         tmp=[]
         if elem[0] in famous:
-            tmp.append(map(float,[elem[1],elem[3],elem[4]]))
+            #tmp.append(map(float,[elem[1],elem[3],elem[4]]))
+            tmp.append(map(float,[elem[1],elem[4]]))
             tmp.append('famous')
         else:
-            tmp.append(map(float,[elem[1],elem[3],elem[4]]))
+            #tmp.append(map(float,[elem[1],elem[3],elem[4]]))
+            tmp.append(map(float,[elem[1],elem[4]]))
             tmp.append('normal')
         dataset.append(tmp)
 #print dataset
@@ -33,16 +35,16 @@ for elem in a:
 shuffle(dataset)
 print dataset[0:5]
 clf = RandomForestClassifier(n_jobs=-1)
-clf = DecisionTreeClassifier()
-skpca = PCA(n_components=1)
+#clf = DecisionTreeClassifier()
+#skpca = PCA(n_components=1)
 value = [elem[0] for elem in dataset]
-value = skpca.fit_transform(value)
+#value = skpca.fit_transform(value)
 print value[0:10]
 label = [elem[1] for elem in dataset]
 #with open('/tmp/aa.txt','a+') as fs:
 #    for i in range(len(value)):
 #        fs.write(str(value[i][0])+'\t'+label[i]+'\n')
-value_train, value_test, label_train, label_test = cross_validation.train_test_split(value, label, test_size=0.9)
+value_train, value_test, label_train, label_test = cross_validation.train_test_split(value, label, test_size=0.3)
 sv = SVC()
 clf.fit(value_train,label_train)
 print classification_report(label_test,clf.predict(value_test))
